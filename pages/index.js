2f4923a477f498d33b142f66ae9c8459682ba9f1
Nailsonnela <nailsonnela048@gmail.com>
2024-11-01 00:51:49 +0000

    adicionar scripts ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function home()  {
-     return <h1>fala renato,eu to praticando heim!!!</h1>
-
+     return <h1>fala renato,eu to praticando heim!</h1>
 }
+
+function teste() {
+     console.log9("teste");
+     }
+
+function teste2() {
+     console.log("identificacao errada")
+}
+
+
 export default home;
+
+
+
