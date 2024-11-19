94231ed387103cb67ba3843736ae024b43ff67d0
Nailsonnela <nailsonnela048@gmail.com>
2024-11-19 23:01:41 +0000

    arquivo novo

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,5 @@
-function home()  {
-     return <h1>fala renato,eu to praticando heim!</h1>
+function home() {
+  return <h1>fala renato,eu to praticando heim!</h1>;
 }
-
-function teste() {
-     console.log9("teste");
-     }
-
-function teste2() {
-     console.log("identificacao errada")
-}
-
 
 export default home;
-
-
-
